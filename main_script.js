9c81404ee43f9ef66c6ebed1048f8722a7cef2b7
jaysonkwd <73496885+jaysonkwd@users.noreply.github.com>
2022-10-01 01:36:41 +0200

    Update main_script.js

--- main_script.js
+++ main_script.js
@@ -1,16 +1,16 @@
 window.onload = () => { 
 	let main_icon = document.querySelector(".main_icon1");
 	main_icon.addEventListener("click", function(){
-		 window.open("/home/jayson/main/projects_page.html", "_self");
+		 window.open("projects_page.html", "_self");
 	});
 
 	let main_icon2 = document.querySelector(".main_icon2");
 	main_icon2.addEventListener("click", function(){
-		 window.open("/home/jayson/main/about_me_page.html", "_self");
+		 window.open("about_me_page.html", "_self");
 	});
 
 	let main_icon3 = document.querySelector(".main_icon3");
 	main_icon3.addEventListener("click", function(){
-		 window.open("/home/jayson/main/contact_page.html", "_self");
+		 window.open("contact_page.html", "_self");
 	});
 }
